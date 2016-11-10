9d57b16ef3bde8f0966640b3f8240c7ca0162489
Konrad Koper <konrad7005@gmail.com>
2016-11-10 12:40:51 +0100

    scanner

---
+++ src/test/git.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println(a+b);
+		
+	}
+
+}
